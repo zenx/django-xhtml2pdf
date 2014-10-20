08a241880ed514d67db9093787b9912a6a02709d
Antonio Melé <antonio.mele@gmail.com>
2014-10-20 18:26:09 +0200

    fix import for python 3k

--- django_xhtml2pdf/utils.py
+++ django_xhtml2pdf/utils.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 from django.template.context import Context
 from django.template.loader import get_template
 from xhtml2pdf import pisa # TODO: Change this when the lib changes.
-import StringIO
+try:
+    from io import StringIO
+except ImportError:
+    import StringIO
 import os
 
 #===============================================================================
